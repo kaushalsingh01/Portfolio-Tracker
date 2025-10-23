00c0d1b12c01812b194902914bb79b0017c645e8
Kaushal Kishor Singh <kaushalsingh1720@gmail.com>
2025-10-23 15:54:55 +0530

    registered custom user model and forms

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.forms import CustomUserChangeForm, CustomUserCreationForm
+from users.models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = (
+        "email", "first_name", "last_name", "date_of_birth",
+        "is_staff", "is_active"
+    )
+    list_filter = ("is_staff", "is_active", "groups")
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("date_of_birth",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("date_of_birth",)}),
+    )
+
+    search_fields = ("email",)
+    ordering = ("email",)
+
+admin.site.register(CustomUser, CustomUserAdmin)
