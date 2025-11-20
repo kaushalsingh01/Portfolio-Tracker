18add43d2f856b69231d0c4aa826cf2d07d9770a
Kaushal Kishor Singh <kaushalsingh1720@gmail.com>
2025-11-20 20:19:45 +0530

    add the route of stocks to url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from portfolio.views import PortfolioViewset
+from market_data.views import StockViewSet, StockSearchViewset
 from users.views import AuthViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -11,6 +12,8 @@ from rest_framework_simplejwt.views import (
 router = DefaultRouter()
 router.register(r'portfolios', PortfolioViewset, basename='portfolio')
 router.register(r'auth', AuthViewSet, basename='auth')
+router.register(r'stocks', StockViewSet, basename='stock')
+router.register(r'search', StockSearchViewset, basename='stock-search')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
